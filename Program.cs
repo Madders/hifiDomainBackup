d354703907fcb41475f7c0f003d92d7f1099da57
Madders <madders@gmail.com>
2020-08-31 10:41:31 +0100

    hello world

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace hifiDomainBackup
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
